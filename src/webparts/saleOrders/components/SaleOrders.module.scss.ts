72c70a96e069a181d7636d4513aa63455cdaa03c
Maneesha1 <60095368+Maneesha1@users.noreply.github.com>
2020-10-30 11:13:09 +0530

    Add files via upload

---
+++ src/webparts/saleOrders/components/SaleOrders.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./SaleOrders.module.css");
+const styles = {
+  saleOrders: 'saleOrders_4730747c',
+  container: 'container_4730747c',
+  row: 'row_4730747c',
+  column: 'column_4730747c',
+  'ms-Grid': 'ms-Grid_4730747c',
+  title: 'title_4730747c',
+  subTitle: 'subTitle_4730747c',
+  description: 'description_4730747c',
+  button: 'button_4730747c',
+  label: 'label_4730747c'
+};
+
+export default styles;
+/* tslint:enable */
